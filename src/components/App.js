9b097e3b58f44284c972fe06475d3b724ca3a82d
Piojank <98431706+Piojank@users.noreply.github.com>
2022-12-20 19:11:17 +0100

    update file paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ContactForm from "./components/ContactForm/ContactForm";
+import ContactForm from "./ContactForm/ContactForm";
 import PhonebookList from "components/PhonebookList/PhonebookList"
 import { Filter } from "components/Filter/Filter";
 // import { store } from "./app/store";
