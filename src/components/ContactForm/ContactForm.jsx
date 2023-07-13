16bd1d4ac9276ec08c5e4304cb1f6923d3826951
piojank <p.jankowski931026@gmail.com>
2023-07-13 19:23:03 +0200

    ContactForm fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -16,6 +16,7 @@ export const ContactForm = () => {
                     name="name"
                     value={name}
                     onChange={handleChangeInput}
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     placeholder='Enter name'
                     required
@@ -29,6 +30,7 @@ export const ContactForm = () => {
                     name="number"
                     value={number}
                     onChange={handleChangeInput}
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     placeholder='Enter phone number'
                     required
